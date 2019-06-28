04eec397a4cd8b9bd75db89a41c834cbc5b02389
kerti <tigerhad@hotmail.com>
2019-06-28 10:10:13 +0700

    1

--- src/app/modules/main/about/about.component.ts
+++ src/app/modules/main/about/about.component.ts
@@ -13,31 +13,16 @@ export class AboutComponent implements OnInit {
       private aboutService: AboutService
       ) {}
 
-<<<<<<< HEAD
-    aboutuser: any[] = [];
-
-    constructor(private aboutService: AboutService) {}
-
-=======
->>>>>>> 6bc7841988e4b0d1d782d4269fc6f4d30262c4d8
     ngOnInit() {
       this.getAbout();
     }
 
     async getAbout() {
-<<<<<<< HEAD
-      const personId = '1' ;
-      const result: any = await this.aboutService.getUserInfo(personId);
-      if (result.statusCode === 200 && result.rows.length) {
-        this.aboutuser = result.rows;
-        console.log('g', result.rows);
-=======
       const result: any = await this.aboutService.getUserInfo(this.personId);
       if (result.statusCode === 200 && result.user.length) {
         console.log(result.user);
         this.aboutuser = result.user[0];
         console.log('g', this.aboutuser);
->>>>>>> 6bc7841988e4b0d1d782d4269fc6f4d30262c4d8
       }
     }
 
