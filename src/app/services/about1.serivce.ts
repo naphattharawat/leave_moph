a21cf7922e082a0158f489e4068eadf938230a55
kerti <tigerhad@hotmail.com>
2019-06-28 10:11:42 +0700

    1

--- src/app/services/about1.serivce.ts
+++ src/app/services/about1.serivce.ts
@@ -8,11 +8,7 @@ export class AboutService {
 
   constructor(
     private http: HttpClient,
-<<<<<<< HEAD:src/app/services/about.ts
-    @Inject('API_URL') private apiUrl
-=======
     @Inject("API_URL") private apiUrl
->>>>>>> 6bc7841988e4b0d1d782d4269fc6f4d30262c4d8:src/app/services/about1.serivce.ts
    ) {
 
 }
@@ -24,11 +20,7 @@ export class AboutService {
 //   }
 
   getUserInfo(personId) {
-<<<<<<< HEAD:src/app/services/about.ts
-    return this.http.post('http://localhost:3001/user/user-about', {personId})
-=======
     return this.http.get(`http://localhost:3001/user/user-about?personId=${personId}`)
->>>>>>> 6bc7841988e4b0d1d782d4269fc6f4d30262c4d8:src/app/services/about1.serivce.ts
     .toPromise()
     .then(result => result)
     .catch(error => error);
